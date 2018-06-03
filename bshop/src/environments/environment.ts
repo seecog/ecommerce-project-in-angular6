e34ae80aa1d199a8fa880bab6a77ef848be5416f
Suraj <Suraj.Malode@emc.com>
2018-06-03 10:22:55 +0530

    worked on routing and hoisting in firebase

--- bshop/src/environments/environment.ts
+++ bshop/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyD43UZQ1ZTx35iwaZYg2TSp0_uF92lwbXU',
+    authDomain: 'bshop-4d7cd.firebaseapp.com',
+    databaseURL: 'https://bshop-4d7cd.firebaseio.com',
+    projectId: 'bshop-4d7cd',
+    storageBucket: 'bshop-4d7cd.appspot.com',
+    messagingSenderId: '664023882949'
+  }
 };
